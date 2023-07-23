5008774175bf0999df64f44f61bcef607f1a391e
jessicahattig <jessicahattig@gmail.com>
2023-07-23 11:28:57 -0700

    Function 1, test 1, pass.

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,6 @@
-// export class Calc {
-//     constructor()
-// }
+export class Calc {
+    constructor() {
+      this.date = new Date(2023, 6, 23);
+      this.day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][this.date.getDay()];
+    }
+}
